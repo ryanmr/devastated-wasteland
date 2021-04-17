2d96d1f80dc29cdec8edf13816e7ffb7aff2cccb
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-04-17 10:39:49 -0500

    Moves why dw into Colophon

---
+++ components/Colophon.js
@@ -0,0 +1,48 @@
+export function Colophon() {
+  return (
+    <section>
+      <p>
+        Why <strong>devastated wasteland</strong>?{" "}
+        <a
+          className="text-blue-700 hover:underline"
+          href="https://www.kentbeck.com/"
+        >
+          Kent Beck
+        </a>
+        , a signer of the{" "}
+        <a
+          className="text-blue-700 hover:underline"
+          href="https://agilemanifesto.org/"
+        >
+          legendary agile manifesto
+        </a>
+        , responsed with this phrase during{" "}
+        <a
+          className="text-blue-700 hover:underline"
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://builtin.com/software-engineering-perspectives/kent-beck-geeks-gusto-globalization#:~:text=It%E2%80%99s%20a%20devastated%20wasteland."
+        >
+          an interview in 2020
+        </a>
+        . It's an evocative phrase, perfectly placed to get a reader riled up
+        and engaged with the purpose of these processes, practices and
+        principles.
+      </p>
+
+      <blockquote className="px-8 my-4 space-y-4">
+        <p>
+          Q: You signed the Agile Manifesto almost 20 years ago. How do you feel
+          about agile now?
+        </p>
+
+        <p>
+          A: It's a <strong>devastated wasteland</strong>. The life has been
+          sucked out of it. It’s a few religious rituals carried out by people
+          who don’t understand the purpose that those rituals were intended to
+          serve in the first place.
+        </p>
+      </blockquote>
+    </section>
+  );
+}
