c861e4f8d686f1b6f437c2827436de9f66faf0e2
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-04-17 10:40:42 -0500

    Adds divider

---
+++ components/TripleLineDivider.js
@@ -0,0 +1,9 @@
+export function TripleLineDivider() {
+  return (
+    <div className="w-1/2 mx-auto my-2">
+      <div className="h-1 my-1 bg-black bg-opacity-25"></div>
+      <div className="h-1 my-1 bg-black bg-opacity-25"></div>
+      <div className="h-1 my-1 bg-black bg-opacity-25"></div>
+    </div>
+  );
+}
