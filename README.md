20269bf3ec91085e433567a94f40322735db9df5
Ryan Rampersad <ryan.rampersad@gmail.com>
2024-11-22 09:34:12 -0600

    Rewrites codebase into modern codebase

---
+++ README.md
@@ -0,0 +1,5 @@
+# Devastated Wasteland
+
+Powered by Next, on Vercel.
+
+Upgraded to a cutting edge build of Next and React.
---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Devastated Wasteland",
+  description:
+    "It's a devastated wasteland. The life has been sucked out of it. It's a few religious rituals carried out by people who don't understand the purpose that those rituals were intended to serve in the first place.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
